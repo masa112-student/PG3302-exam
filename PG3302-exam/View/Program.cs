314fa2a04240882fc360976bbadfaf945dc7baf7
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2022-10-26 18:29:18 +0200

    Added bullet class

--- PG3302-exam/View/Program.cs
+++ PG3302-exam/View/Program.cs
@@ -6,18 +6,24 @@ namespace View
     {
         static void Main(string[] args) {
             Console.CursorVisible = false;
-            Console.SetWindowSize(50, 20);
-            Console.SetBufferSize(50, 20);
 
 			Enemy enemy = new(Console.BufferWidth);
 
 
 			Player player = new(Console.BufferWidth);
+
+            Bullet? bullet = null;
+
             while (true) {
                Console.Clear();
 
                 player.Draw();
                 enemy.Draw();
+                if(bullet != null)
+                {
+                    bullet.Draw();
+                }
+           
                 Thread.Sleep(100);
 
 
@@ -28,11 +34,30 @@ namespace View
 					{
 						player.XPos -= 1;
 					}
-					else if (keypress.Key == ConsoleKey.D)
+                    if (keypress.Key == ConsoleKey.Spacebar)
+                    {
+                        bullet = new Bullet(Console.WindowHeight, player.XPos);
+                    }
+                    else if (keypress.Key == ConsoleKey.D)
 					{
 						player.XPos += 1;
 					}
 				}
+
+                /*if (Console.KeyAvailable)
+                {
+                    var keypress = Console.ReadKey(true);
+
+                    if (keypress.Key == ConsoleKey.Spacebar)
+                    {
+                        bullet = new Bullet(Console.WindowHeight, player.XPos);
+                    }
+                  
+                }
+                */
+
+              
+
 
                 
             }
