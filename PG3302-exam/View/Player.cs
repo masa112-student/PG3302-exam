1941527446293f2f4e7085d074acae2d5b508c66
Martin <martinsagbakk@gmail.com>
2022-10-26 16:55:06 +0200

    Created basic proof of consept player

---
+++ PG3302-exam/View/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace View
+{
+    internal class Player
+    {
+        private readonly int _maxWidth;
+
+        private int _xPos;
+
+        public int SpirteHeight { get; set; }
+        public int SpirteWidth { get; set; }
+
+        public int XPos {
+            get => _xPos;
+            set {
+                if(value <= 0 )
+                    _xPos = 0;
+                else if (value >= _maxWidth-SpirteWidth)
+                    _xPos = _maxWidth - SpirteWidth;
+                else 
+                    _xPos = value;
+            }
+        }
+
+        public Player(int maxWidth) {
+            _maxWidth = maxWidth;
+
+            XPos = _maxWidth / 2;
+
+            SpirteHeight = 2;
+            SpirteWidth = 2;
+            
+        }
+        public void Draw() {
+            Console.SetCursorPosition(XPos, (Console.WindowHeight - 2));
+            Console.Write("P");
+            Console.Write("P");
+            Console.SetCursorPosition(XPos, (Console.WindowHeight - 1));
+
+            Console.Write("P");
+            Console.Write("P");
+        }
+    }
+}
--- PG3302-exam/View/Program.cs
+++ PG3302-exam/View/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace View
+﻿using System.Diagnostics;
+
+namespace View
 {
     internal class Program
     {
         static void Main(string[] args) {
-            
+            Console.CursorVisible = false;
+            Console.SetWindowSize(50, 20);
+            Console.SetBufferSize(50, 20);
+
+            Player player = new(Console.BufferWidth);
+            while (true) {
+                Console.Clear();
+
+                player.Draw();
+
+                var keypress = Console.ReadKey(true);
+
+                if (keypress.Key == ConsoleKey.A) {
+                    player.XPos -= 1;
+                } else if (keypress.Key == ConsoleKey.D) {
+                    player.XPos += 1;                
+                }
+            }
         }
     }
 }
