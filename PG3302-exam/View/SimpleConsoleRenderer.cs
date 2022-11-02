5214634009534601ef2bc8a7b3d5149d3be36d4e
Martin <martinsagbakk@gmail.com>
2022-11-02 14:51:30 +0100

    Added simple implemetations of Renderer and UserInput

---
+++ PG3302-exam/View/SimpleConsoleRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace View
+{
+    public class SimpleConsoleRenderer : IRenderer
+    {
+        public SimpleConsoleRenderer() { 
+            Console.CursorVisible = false;
+        }
+        ~SimpleConsoleRenderer() {
+            Console.CursorVisible = true;
+        }
+
+        public void DrawChar(int x, int y, char c) {
+            if(x < Console.BufferWidth && y < Console.BufferHeight) {
+               Console.SetCursorPosition(x, y);
+               Console.Write(c);
+            }
+        }
+
+        public void DrawSprite(Sprite sprite) {
+            string[] lines = sprite.Data.Split("\n");
+            int yPos = sprite.Pos.Y;
+            foreach(string line in lines) {
+                if (sprite.Pos.X >= Console.BufferWidth || yPos >= Console.BufferHeight) 
+                    break;
+
+                Console.SetCursorPosition(sprite.Pos.X, yPos);
+                Console.Write(line);
+                yPos++;
+            }
+        }
+    }
+}
---
+++ PG3302-exam/View/SimpleConsoleUserInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    public class SimpleConsoleUserInput : IUserInput
+    {
+        private ConsoleKey? _keyDown = null;
+
+        private Stopwatch sw;
+        private double pollRateMs = 35;
+
+        public SimpleConsoleUserInput() {
+            sw = Stopwatch.StartNew();
+        }
+
+        public bool IsKeyDown(ConsoleKey key) {
+            if (Console.KeyAvailable) {
+                _keyDown = Console.ReadKey(true).Key;
+                sw.Restart();
+            }
+            else if (sw.ElapsedMilliseconds > pollRateMs) {
+                _keyDown = null;
+                sw.Restart();
+            }
+
+            return key == _keyDown;
+        }
+
+
+    }
+}
