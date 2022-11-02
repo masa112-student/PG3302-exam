bc68f64d1d40635e8fb95a90df407f19ed6ff5bb
Martin <martinsagbakk@gmail.com>
2022-11-02 14:48:38 +0100

    Added IUserInput and IRenderer interfaces

---
+++ PG3302-exam/View/IRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace View
+{
+    public interface IRenderer
+    {
+        void DrawChar(int x, int y, char c);
+        void DrawSprite(Sprite sprite);
+    }
+}
