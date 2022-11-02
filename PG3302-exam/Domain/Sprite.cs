8a743534e6e8f1ac7af81f7d8d955b9b59ad0a8d
Martin <martinsagbakk@gmail.com>
2022-11-02 14:48:16 +0100

    Added new constructors to Sprite

--- PG3302-exam/Domain/Sprite.cs
+++ PG3302-exam/Domain/Sprite.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace View
 {
-    internal class Sprite
+    public class Sprite
     {
+        public string Data { get; private set; }
+        public Point Pos { get; set; }
+
+        public Sprite(string data, Point pos) {
+            Data = data;
+            Pos = pos;
+        }
+        public Sprite(string data = ""):this(data, new Point(0,0)) { }
     }
 }
