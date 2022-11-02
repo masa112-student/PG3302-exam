9ebba98dc58bca06f50d806cbb23f5177701eb13
Martin <martinsagbakk@gmail.com>
2022-11-02 14:47:15 +0100

    Added + operator to point

--- PG3302-exam/Domain/Point.cs
+++ PG3302-exam/Domain/Point.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace View
 {
-    internal struct Point
+    public struct Point
     {
         public int X;
         public int Y;
 
         public Point(int x, int y) {
             X = x; Y = y;   
+        }
+
+        public static Point operator+(Point a, Point b) {
+            return new Point(a.X + b.X, a.Y + b.Y);
         }
 
     }
