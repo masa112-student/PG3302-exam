01466b3f580f24dfca4cd93d9a965468644ee7de
Martin <martinsagbakk@gmail.com>
2022-11-06 15:23:01 +0100

    Added sprite to baseEnemy

--- PG3302-exam/Domain/BaseEnemy.cs
+++ PG3302-exam/Domain/BaseEnemy.cs
@@ -8,12 +8,25 @@ namespace Domain
 {
     internal class BaseEnemy
     {
-        public Point pos { get; set; }
+        private Sprite activeSprite;
 
-        public void Move() { }
-        public Sprite GetSprite() {
-            return new Sprite();
+        public Point pos { get; set; }
+        public Sprite ActiveSprite { 
+            get { 
+                activeSprite.Pos = pos;
+                return activeSprite;
+            }
+            set {
+                if(value != null) 
+                    activeSprite = value;
+            }
         }
+
+        public BaseEnemy() {
+            ActiveSprite = new Sprite();
+        }
+        public void Move() { }
+
         public Bullet Attack() {
             return new Bullet(0, 0, 0);
         }
