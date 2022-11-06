929f6a288beb9912919dedd7ddccdce4c0429785
Martin <martinsagbakk@gmail.com>
2022-11-06 15:25:47 +0100

    Changed point to be a class

--- PG3302-exam/Domain/Point.cs
+++ PG3302-exam/Domain/Point.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public struct Point
+    public class Point
     {
         public int X;
         public int Y;
@@ -14,9 +14,13 @@ namespace Domain
         public Point(int x, int y) {
             X = x; Y = y;   
         }
+        public Point() {}
 
         public static Point operator+(Point a, Point b) {
             return new Point(a.X + b.X, a.Y + b.Y);
+        }
+        public static Point operator-(Point a, Point b) {
+            return new Point(a.X - b.X, a.Y - b.Y);
         }
 
     }
