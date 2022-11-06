becb238d9b1bc82b39a3165fc2a291d3ea0df89f
Martin <martinsagbakk@gmail.com>
2022-11-06 12:48:48 +0100

    Added IGameBoard & MockGameBoard

---
+++ PG3302-exam/Domain/IGameBoard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTypes;
+
+namespace Domain
+{
+    public interface IGameBoard
+    {
+        public int Score { get; set; }
+        public bool IsGameActive { get; set; }
+        public void Update();
+        public List<Sprite> GetSprites();
+    }
+}
---
+++ PG3302-exam/Domain/MockGameBoard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTypes;
+
+namespace Domain
+{
+    public class MockGameBoard : IGameBoard
+    {
+        public int Score { get; set; }
+        public bool IsGameActive { get; set; }
+
+        public MockGameBoard() {
+            IsGameActive = true;
+        }
+
+        public List<Sprite> GetSprites() {
+            return new List<Sprite>();
+        }
+
+        public void Update() {
+            
+        }
+    }
+}
