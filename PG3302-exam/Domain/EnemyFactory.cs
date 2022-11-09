7faf2a4dfcdbc6b8fc23327787124a8fde900bb0
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2022-11-09 15:28:18 +0100

    Factory for enemies (2)

--- PG3302-exam/Domain/EnemyFactory.cs
+++ PG3302-exam/Domain/EnemyFactory.cs
@@ -3,8 +3,19 @@ namespace Domain
 {
     public class EnemyFactory
     {
-        public EnemyFactory()
+        public static IEnemy getEnemy(BaseEnemy enemyType)
         {
+            IEnemy? enemy = null;
+            BaseEnemy baseEnemy = new BaseEnemy();
+
+            if (enemyType.Equals(baseEnemy)) {
+                enemy = new BaseEnemy();
+            }
+            else if (enemyType.Equals(MoreSpeedEnemyDecorator(baseEnemy));
+            {
+                enemy = new MoreSpeedEnemyDecorator(baseEnemy);
+            }
+            return enemy;
         }
     }
 }
