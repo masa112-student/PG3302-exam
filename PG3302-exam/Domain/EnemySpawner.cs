cea970776c00a46fc0f2c4ec0d93fd82cfc5a168
Martin <martinsagbakk@gmail.com>
2022-11-10 22:04:19 +0100

    Added enemy factory to enemy spawner

--- PG3302-exam/Domain/EnemySpawner.cs
+++ PG3302-exam/Domain/EnemySpawner.cs
@@ -1,34 +1,51 @@
 ﻿using Domain.Enemies;
+using System.Collections.Immutable;
 
 namespace Domain
 {
 	public class EnemySpawner
 	{
-		private BoardDimensions _boardDimensions;
 
-		private List<BaseEnemy> _enemies = new();
+		private List<Enemy> _enemies = new();
 
 		private static Random random = new Random();
 
 		private Point enemyStartPos = new Point(0, 4);
-        private Sprite enemySprite = new Sprite("1------2------3\n      \\4/      ");
-        
-		public EnemySpawner(BoardDimensions boardDimensions) {
+        private Sprite enemySprite = new Sprite("xxx\n\\x/");
+
+		private List<EnemyType> _enemyTypes = new () { EnemyType.Basic };
+
+        private BoardDimensions _boardDimensions;
+		private EnemyFactoryMaker _enemyFactoryMaker;
+
+        public EnemySpawner(BoardDimensions boardDimensions) {
             _boardDimensions = boardDimensions;
+			_enemyFactoryMaker = new(boardDimensions);
         }
+
+		public void AddTypeToSpawnPool(EnemyType type) {
+			//if (!_enemyTypes.Contains(type)) {
+				_enemyTypes.Add(type);
+			//}
+		}
 
         public void EnemySpawnChecker() {
 			_enemies = new();
-			int randomInt = random.Next(1, 4);
+			int enemyCount = random.Next(1, 4);
 
-			for (int i = 0; i < randomInt; i++) {
-				BaseEnemy enemy = new BaseEnemy(_boardDimensions);
+			EnemyType typeToSpawn;
+			int typeToSpawnIndex;
+			for (int i = 0; i < enemyCount; i++) {
+				typeToSpawnIndex = random.Next(_enemyTypes.Count);
+				typeToSpawn = _enemyTypes[typeToSpawnIndex];
+
+                Enemy enemy = _enemyFactoryMaker.GetFactory(typeToSpawn).getEnemy();
 				enemy.Pos = enemyStartPos + new Point(i * (enemySprite.Size.Width + 1), 0);
 				enemy.ActiveSprite = new Sprite(enemySprite);
 				_enemies.Add(enemy);
 			}
 		}
-		public List<BaseEnemy> Enemies {
+		public List<Enemy> Enemies {
 			get { return _enemies; }
 		}
 
