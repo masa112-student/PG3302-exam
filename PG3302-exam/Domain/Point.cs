d876387a7157b1cb98b182b74b90f6e9aac7576a
Martin <martinsagbakk@gmail.com>
2022-11-10 22:02:23 +0100

    Added operator for muliplying a point and an int

--- PG3302-exam/Domain/Point.cs
+++ PG3302-exam/Domain/Point.cs
@@ -20,11 +20,15 @@
             Point p = (Point)obj;
             return X == p.X && Y == p.Y;
         }
+
         public static Point operator +(Point a, Point b) {
             return new Point(a.X + b.X, a.Y + b.Y);
         }
         public static Point operator -(Point a, Point b) {
             return new Point(a.X - b.X, a.Y - b.Y);
+        }
+        public static Point operator *(Point a, int b) {
+            return new Point(a.X * b, a.Y * b);
         }
         public static bool operator ==(Point? a, Point? b) {
             if (a is null)
