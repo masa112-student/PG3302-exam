b85fe8cd566ac99db7b2f2c7725871b6a1e91bd9
Martin <martinsagbakk@gmail.com>
2022-11-12 15:22:39 +0100

    Added IMovable interface

---
+++ PG3302-exam/Domain/IMovable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public interface IMovable {
+        public int Speed { get; set; }
+        public Point Pos { get; set; }
+    }
+}
