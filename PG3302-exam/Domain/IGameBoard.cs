f7b0c00c1f891b481ef557bc3b9006ab594b7df4
Martin <martinsagbakk@gmail.com>
2022-11-12 15:19:36 +0100

    Added a summary to IGameBoard

--- PG3302-exam/Domain/IGameBoard.cs
+++ PG3302-exam/Domain/IGameBoard.cs
@@ -1,5 +1,19 @@
 ﻿namespace Domain
 {
+    /// <summary>
+    /// The board where all the game logic takes place
+    /// 
+    /// Start() will create/reset the board.
+    /// Update() is where all the changes takes place, and should be called in a loop for as long as the game is active.
+    /// MovePlayer() and Playerattack() tells the class that during the next Update() call, the player should move or attack.
+    /// GetSprites() Will return all sprites currently on the screen, with all the positional data set.
+    /// 
+    /// 
+    /// Note:
+    /// Technically this does not need to be an interface, as it is unlikley that there will ever be more than one implementation for the gameboard.
+    /// However by relying only on the interface in the view it makes i much easier to create mock implemetaions either for testing,
+    /// or during development when the different modules might be worked on by different members of the team.
+    /// </summary>
     public interface IGameBoard
     {
         public enum MoveDir
