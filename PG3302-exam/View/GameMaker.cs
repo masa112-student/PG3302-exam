b78c1e20b91776efe2b6aa09bd042e133d14c14f
Martin <martinsagbakk@gmail.com>
2022-11-12 15:31:47 +0100

    Added GameMaker Factory to create a game based on the OS of the user

---
+++ PG3302-exam/View/GameMaker.cs
@@ -0,0 +1,36 @@
+﻿using Domain;
+using Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    internal class GameMaker
+    {
+        public static GameManager MakeGame() {
+            GameManager manager;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                manager = new GameManager(
+                new SimpleConsoleRenderer(),
+                new WindowsConsoleUserInput(),
+                new GameBoard(Console.WindowWidth, Console.WindowHeight),
+                new JsonPersistance()
+                );
+            }
+            else {
+                manager = new GameManager(
+                new SimpleConsoleRenderer(),
+                new SimpleConsoleUserInput(),
+                new GameBoard(Console.WindowWidth, Console.WindowHeight),
+                new JsonPersistance()
+                );
+            }
+
+            return manager; 
+        }
+    }
+}
--- PG3302-exam/View/Program.cs
+++ PG3302-exam/View/Program.cs
@@ -7,23 +7,7 @@ namespace View
     internal class Program
     {
         static void Main(string[] args) {
-            GameManager manager;
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
-                manager = new GameManager(
-                new SimpleConsoleRenderer(),
-                new WindowsConsoleUserInput(),
-                new GameBoard(Console.WindowWidth, Console.WindowHeight),
-                new JsonPersistance()
-                );
-            }
-            else {
-                manager = new GameManager(
-                new SimpleConsoleRenderer(),
-                new SimpleConsoleUserInput(),
-                new GameBoard(Console.WindowWidth, Console.WindowHeight),
-                new JsonPersistance()
-                );
-            }
+            GameManager manager = GameMaker.MakeGame();
             manager.StartupView();
         }
 
