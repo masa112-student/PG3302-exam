ef9bd1196b28e0c8d7f6df937d3ef5e0b55d8426
Martin <martinsagbakk@gmail.com>
2022-11-13 16:29:45 +0100

    Updated Equality check for Point

--- PG3302-exam/Domain/Point.cs
+++ PG3302-exam/Domain/Point.cs
@@ -10,13 +10,10 @@
         public int X { get; set; }
         public int Y { get; set; }
 
-        public override bool Equals(object? obj) {
-            if (obj == null || GetType() != obj.GetType()) {
-                return false;
-            }
-            Point p = (Point)obj;
-            return X == p.X && Y == p.Y;
-        }
+        public override bool Equals(object? obj) => obj is Point p && Equals(p);
+        public bool Equals(Point p) => X == p.X && Y == p.Y;
+
+        public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode();
 
         public static Point operator +(Point a, Point b) {
             return new Point(a.X + b.X, a.Y + b.Y);
@@ -27,15 +24,12 @@
         public static Point operator *(Point a, int b) {
             return new Point(a.X * b, a.Y * b);
         }
-        public static bool operator ==(Point? a, Point? b) {
-            if (a is null)
-                return b is null;
+        public static bool operator ==(Point a, Point b) {
             return a.Equals(b);
         }
-        public static bool operator !=(Point? a, Point? b) {
-            if (a is null)
-                return b is not null;
+        public static bool operator !=(Point a, Point b) {
             return !a.Equals(b);
         }
+
     }
 }
