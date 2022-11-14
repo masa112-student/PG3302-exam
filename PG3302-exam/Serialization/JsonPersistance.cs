95b6df1b659a21806fe5a9afed6e1159949dc986
Martin <martinsagbakk@gmail.com>
2022-11-15 00:12:25 +0100

    Changed JsonPersistance to use IFileSystem, added constructor that accepts it as an argumetn

--- PG3302-exam/Serialization/JsonPersistance.cs
+++ PG3302-exam/Serialization/JsonPersistance.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.IO.Abstractions;
 using System.Text;
 using System.Text.Json;
 
@@ -8,16 +9,20 @@ namespace Serialization
     public class JsonPersistance : IPersistance
     {
         private string _fileName;
+        private readonly IFileSystem _fileSystem;
 
-        public JsonPersistance(string filename) {
+        public JsonPersistance(string filename) : this(filename, new FileSystem()) { }
+
+        public JsonPersistance(string filename, IFileSystem fileSystem) {
             _fileName = filename;
+            _fileSystem = fileSystem;
         }
 
         public HighScores LoadHighScores() {
-            if (!File.Exists(_fileName))
+            if (!_fileSystem.File.Exists(_fileName))
                 return new(new List<Score>());
 
-            string jsonScores = File.ReadAllText(_fileName, Encoding.UTF8);
+            string jsonScores = _fileSystem.File.ReadAllText(_fileName, Encoding.UTF8);
             if (string.IsNullOrEmpty(jsonScores))
                 return new(new List<Score>());
 
@@ -28,9 +33,9 @@ namespace Serialization
         }
 
         public void SaveHighScores(HighScores highScores) {
-            string jsonScores = JsonSerializer.Serialize(highScores.Scores);
+            string jsonScores = JsonSerializer.Serialize(highScores);
             try {
-                File.WriteAllText(_fileName, jsonScores);
+                _fileSystem.File.WriteAllText(_fileName, jsonScores);
             }
             catch (UnauthorizedAccessException) {
                 Trace.TraceError("The application does not have access to the highscore file");
