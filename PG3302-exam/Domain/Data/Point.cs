8abc456c944322eee463848637ad579017d0d932
Martin <martinsagbakk@gmail.com>
2022-11-16 12:21:34 +0100

    Simplifide newing in operators

--- PG3302-exam/Domain/Data/Point.cs
+++ PG3302-exam/Domain/Data/Point.cs
@@ -15,10 +15,10 @@
 
         public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode();
 
-        public static Point operator +(Point a, Point b) => new Point(a.X + b.X, a.Y + b.Y);
-        public static Point operator -(Point a, Point b) => new Point(a.X - b.X, a.Y - b.Y);
-        public static Point operator *(Point a, int b) => new Point(a.X * b, a.Y * b);
-        public static Point operator /(Point a, int b) => new Point(a.X / b, a.Y / b);
+        public static Point operator +(Point a, Point b) => new(a.X + b.X, a.Y + b.Y);
+        public static Point operator -(Point a, Point b) => new(a.X - b.X, a.Y - b.Y);
+        public static Point operator *(Point a, int b) => new(a.X * b, a.Y * b);
+        public static Point operator /(Point a, int b) => new(a.X / b, a.Y / b);
         public static bool operator ==(Point a, Point b) => a.Equals(b);
         public static bool operator !=(Point a, Point b) => !a.Equals(b);
     }
