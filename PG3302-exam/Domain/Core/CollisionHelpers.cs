d5112d6ed46607d0f00f3e146bad6e977fde9c44
Martin <martinsagbakk@gmail.com>
2022-11-16 13:33:30 +0100

    Added Collisions helper a class for collecting collision logic

---
+++ PG3302-exam/Domain/Core/CollisionHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Core
+{
+    internal class CollisionHelpers
+    {
+
+        public static bool AABBHit(IHittable a, IHittable b) {
+            if(a.Mask != b.Mask)
+                return false;
+
+            return a.Pos.X < b.Pos.X + b.Size.Width &&
+                a.Pos.Y < b.Pos.Y + b.Size.Height &&
+                a.Pos.X + a.Size.Width > b.Pos.X &&
+                a.Pos.Y + a.Size.Height > b.Pos.Y;
+        }
+    }
+}
