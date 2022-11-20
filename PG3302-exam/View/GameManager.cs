011be8820a9c62d2836657e2a0e594bd8e99dcdf
Martin <martinsagbakk@gmail.com>
2022-11-20 12:51:28 +0100

    Moved UserInputFormatter to a new file

--- PG3302-exam/View/GameManager.cs
+++ PG3302-exam/View/GameManager.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Text;
 
 using Domain.Data;
 using Domain.Core;
@@ -156,33 +155,5 @@ namespace View
             _renderer.ClearScreen();
         }
 
-    }
-}
-
-
-class UserInputFormatter
-{
-    private StringBuilder _input = new StringBuilder();
-
-    public char LastReadChar { get; private set; }
-    public bool UserHitEnter { get; private set; }  
-
-    public void AddInput(char c) {
-        LastReadChar = c;
-        
-        if (c == (char)ConsoleKey.Backspace && _input.Length > 0)
-            _input.Remove(_input.Length - 1, 1);
-        else if(char.IsLetterOrDigit(c) || c == ' ')
-            _input.Append(c);
-
-        UserHitEnter = (c == (char)ConsoleKey.Enter);
-    }
-
-    public string GetInputString() {
-        _input.Replace("\r", string.Empty);
-        _input.Replace("\n'", string.Empty);
-        _input.Replace("\b", string.Empty);
-
-        return _input.ToString();
     }
 }
---
+++ PG3302-exam/View/UserInputFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+class UserInputFormatter
+{
+    private StringBuilder _input = new StringBuilder();
+
+    public char LastReadChar { get; private set; }
+    public bool UserHitEnter { get; private set; }  
+
+    public void AddInput(char c) {
+        LastReadChar = c;
+        
+        if (c == (char)ConsoleKey.Backspace && _input.Length > 0)
+            _input.Remove(_input.Length - 1, 1);
+        else if(char.IsLetterOrDigit(c) || c == ' ')
+            _input.Append(c);
+
+        UserHitEnter = (c == (char)ConsoleKey.Enter);
+    }
+
+    public string GetInputString() {
+        _input.Replace("\r", string.Empty);
+        _input.Replace("\n'", string.Empty);
+        _input.Replace("\b", string.Empty);
+
+        return _input.ToString();
+    }
+}
